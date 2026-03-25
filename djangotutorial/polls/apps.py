376636ca4d0c7219a6b6b1947f1003a649f55e31
deborq67 <debohorquez@mail.fhsu.edu>
2026-03-24 19:59:07 -0500

    Changed a few things on the database for my version of Django.

--- djangotutorial/polls/apps.py
+++ djangotutorial/polls/apps.py
@@ -3,3 +3,13 @@ from django.apps import AppConfig
 
 class PollsConfig(AppConfig):
     name = 'polls'
+
+INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
